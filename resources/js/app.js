b091fbeaf48b7762cb3cbc1461ed3bcb28a11132
Themba Real Lukhele <realthemba@gmail.com>
2024-04-15 20:31:46 +0200

    configuring react components

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
 import './bootstrap';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import YourComponent from './components/YourComponent';
+
+ReactDOM.render(
+    // <YourComponent />,
+    document.getElementById('root')
+);
